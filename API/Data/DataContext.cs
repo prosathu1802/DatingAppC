086eaf18b388ee0999284f178c9a9d4448c0bb7d
Hin Pham <hinphamd0218@gmail.com>
2023-07-12 16:52:49 -0500

    End of Section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,25 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(key => new {key.SourceUserId, key.TargetUserId});
+
+            builder.Entity<UserLike>()
+                .HasOne(source => source.SourceUser)
+                .WithMany(like => like.LikedUsers)
+                .HasForeignKey(source => source.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(target => target.TargetUser)
+                .WithMany(like => like.LikedByUsers)
+                .HasForeignKey(target => target.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
